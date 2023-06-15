d4912032dad4391a5a9824d83b1db250befc51a5
qpeng044 <qingyong.peng@trifo.com>
2023-06-16 01:08:13 +0800

    add point to path map

--- datafusion_simulink.py
+++ datafusion_simulink.py
@@ -2,15 +2,17 @@ import dearpygui.dearpygui as dpg
 from multiprocessing import Process, Queue, Lock
 import time
 import sched
-gui_label_ch = ["开始", "添加路径", "提取数据", "画图", "打开多图模式", "关闭多图模式", "日志"]
-gui_label_en = ["Start", "AddPath", "InputPatten",
+gui_label_ch = ["开始", "添加路径", "清除路径", "画图", "打开多图模式", "关闭多图模式", "日志"]
+gui_label_en = ["Start", "AddPath", "DeletePath",
                 "plot", "OpenMultiPlot", "CloseMultiPlot", "Logger"]
 
-gui_label_tag = ["start", "add_path", "input_patten",
+gui_label_tag = ["start", "add_path", "delete_path",
                  "plot", "open_multplot", "close_multplot", "logger"]
 
 
 class App:
+    is_init_motion_handle = 0
+
     def __init__(self) -> None:
         self.Gui()
         word_simulink = WorldCoordinate()
@@ -26,13 +28,23 @@ class App:
                 dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
         dpg.bind_font(default_font)  # Binds the font globally
         with dpg.window(tag="Primary Window"):
+            global head_direction_data, head_speed_data, control_time, control_robot_position_data
             with dpg.group(horizontal=True):
                 dpg.add_button(label=gui_label_ch[0],
                                tag=gui_label_tag[0])
                 dpg.add_button(label=gui_label_ch[1],
                                tag=gui_label_tag[1], callback=self.InitRobotMotion)
-            dpg.add_plot(tag="simulink_plot",
-                         width=dpg.get_viewport_width()-10, height=dpg.get_viewport_height()-dpg.get_item_pos(gui_label_tag[0])[1]-50)
+            with dpg.plot(tag="simulink_plot",
+                          width=dpg.get_viewport_width()-10, height=dpg.get_viewport_height()-dpg.get_item_pos(gui_label_tag[0])[1]-50):
+                dpg.add_plot_legend()
+
+                # REQUIRED: create x and y axes
+                dpg.add_plot_axis(dpg.mvXAxis, label="x",
+                                  tag=f"simulink_plot_x_axis")
+                dpg.add_plot_axis(dpg.mvYAxis, label="y",
+                                  tag=f"simulink_plot_y_axis")
+                dpg.add_line_series(
+                    x=control_robot_position_data[0], y=control_robot_position_data[1], tag="raw_path", parent=f"simulink_plot_y_axis")
 
         dpg.set_viewport_resize_callback(self.viewport_resize_callback)
         dpg.show_viewport()
@@ -46,14 +58,48 @@ class App:
         )-dpg.get_item_pos(gui_label_tag[0])[1]-50)
 
     def InitRobotMotion(self, sender, appdata):
-        with dpg.window(tag="robot_motion"):
-            dpg.add_plot(tag="motion_map")
-            with dpg.item_handler_registry(tag="add_point_to_map") as handler:
-                dpg.add_item_clicked_handler(callback=self.AddPointToMap)
-            dpg.bind_item_handler_registry("motion_map", "add_point_to_map")
-            dpg.add_line_series("motion_map", tag="head_direction_data")
+        with dpg.window(tag="robot_motion", on_close=self.CloseCallback):
+            dpg.add_button(label=gui_label_ch[2],
+                           tag=gui_label_tag[2], callback=self.ClearPointToMap)
+            with dpg.plot(tag="motion_map"):
+                dpg.add_plot_legend()
 
-            dpg.add_table(tag="point_table")
+                # REQUIRED: create x and y axes
+                dpg.add_plot_axis(dpg.mvXAxis, label="x",
+                                  tag=f"x_axis")
+                dpg.add_plot_axis(dpg.mvYAxis, label="y",
+                                  tag=f"y_axis")
+                dpg.add_line_series(
+                    x=control_robot_position_data[0], y=control_robot_position_data[1], tag="head_direction_data", parent=f"y_axis")
+            if(self.is_init_motion_handle == 0):
+                self.is_init_motion_handle = 1
+                with dpg.item_handler_registry(tag="add_point_to_map") as handler:
+                    dpg.add_item_clicked_handler(callback=self.AddPointToMap)
+
+                with dpg.item_handler_registry(tag="robot_motion_resize") as handler:
+                    dpg.add_item_resize_handler(
+                        callback=self.RobotMotionResizeCallback)
+            dpg.bind_item_handler_registry(
+                "robot_motion", "robot_motion_resize")
+            dpg.bind_item_handler_registry(
+                "motion_map", "add_point_to_map")
+            # dpg.add_table(tag="point_table")
+
+    def ClearPointToMap(self, sender, appdata):
+        global head_direction_data, head_speed_data, control_time, control_robot_position_data
+        control_robot_position_data = [[0], [0]]
+        dpg.set_value("head_direction_data", [
+                      control_robot_position_data[0], control_robot_position_data[1]])
+
+    def CloseCallback(self, sender, appdata):
+        global head_direction_data, head_speed_data, control_time, control_robot_position_data
+        if(sender == "robot_motion"):
+            dpg.set_value("raw_path", [
+                control_robot_position_data[0], control_robot_position_data[1]])
+        dpg.delete_item(sender)
+
+    def RobotMotionResizeCallback(self, send, appdata):
+        dpg.set_item_width("motion_map", dpg.get_item_width("robot_motion")-10)
 
     def AddPointToMap(self, sender, appdata):
         global head_direction_data, head_speed_data, control_time, control_robot_position_data
@@ -63,6 +109,7 @@ class App:
         print(control_robot_position_data)
         dpg.set_value("head_direction_data", [
                       control_robot_position_data[0], control_robot_position_data[1]])
+        
 
 
 head_direction_data = []
