7e073ca88248445f3433081541027d4ab3603031
qpeng044 <qingyong.peng@trifo.com>
2023-07-03 10:24:45 +0800

    test for gui color of button

--- test2.py
+++ test2.py
@@ -2,29 +2,46 @@ import dearpygui.dearpygui as dpg
 
 dpg.create_context()
 
-with dpg.window(label="Tutorial"):
+with dpg.window(label="Tutorial", pos=(20, 50), width=275, height=225) as win1:
+    t1 = dpg.add_input_text(default_value="some text")
+    t2 = dpg.add_input_text(default_value="some text")
+    with dpg.child_window(height=100):
+        t3 = dpg.add_input_text(default_value="some text")
+        dpg.add_input_int()
+    dpg.add_input_text(default_value="some text")
 
-    with dpg.table(header_row=False, row_background=True,
-                   borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                   borders_outerV=True):
+with dpg.window(label="Tutorial", pos=(320, 50), width=275, height=225) as win2:
+    dpg.add_input_text(default_value="some text")
+    dpg.add_input_int()
 
-        # use add_table_column to add columns to the table,
-        # table columns use child slot 0
-        dpg.add_table_column()
-        dpg.add_table_column()
-        dpg.add_table_column()
+with dpg.theme() as global_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
 
-        # add_table_next_column will jump to the next row
-        # once it reaches the end of the columns
-        # table next column use slot 1
-        for i in range(0, 4):
-            with dpg.table_row():
-                for j in range(0, 3):
-                    if j == 2:
-                        dpg.add_drag_float(
-                            tag=f"Row{i} Column{j}", max_value=5.0)
-                    else:
-                        dpg.add_text(f"Row{i} Column{j}")
+    with dpg.theme_component(dpg.mvInputInt):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (140, 255, 23), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+with dpg.theme() as container_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (150, 100, 100), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+    with dpg.theme_component(dpg.mvInputInt):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (100, 150, 100), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+with dpg.theme() as item_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (200, 200, 100), category=dpg.mvThemeCat_Core)
+        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
+
+dpg.bind_theme(global_theme)
+dpg.bind_item_theme(win1, container_theme)
+dpg.bind_item_theme(t2, item_theme)
+
+dpg.show_style_editor()
 
 dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
